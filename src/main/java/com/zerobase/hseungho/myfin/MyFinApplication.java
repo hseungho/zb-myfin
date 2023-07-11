320bb8cc3eb5480f5bd22ca3ed151cbf6c0ce77b
hseungho <hsh47607@naver.com>
2023-07-12 00:55:07 +0900

    first commit

---
+++ src/main/java/com/zerobase/hseungho/myfin/MyFinApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.hseungho.myfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/hseungho/myfin/MyFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.hseungho.myfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
