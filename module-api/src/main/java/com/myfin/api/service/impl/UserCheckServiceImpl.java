8cc7b20aba11a688996804bbc4376b68ac61a027
hseungho <hsh47607@naver.com>
2023-07-18 15:22:46 +0900

    Add JPA query for existbyuserid into UserRepository

--- module-api/src/main/java/com/myfin/api/service/impl/UserCheckServiceImpl.java
+++ module-api/src/main/java/com/myfin/api/service/impl/UserCheckServiceImpl.java
@@ -1,10 +1,21 @@
 package com.myfin.api.service.impl;
 
 import com.myfin.api.service.UserCheckService;
+import com.myfin.core.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserCheckServiceImpl implements UserCheckService {
+
+    private final UserRepository userRepository;
+
     @Override
     public boolean checkUserIdAvailable(String userId) {
         return false;
     }
+
 }
