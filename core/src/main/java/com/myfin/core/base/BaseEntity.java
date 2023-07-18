c1f0c16835afd4901edadc68a6625429c67432b8
hseungho <hsh47607@naver.com>
2023-07-18 14:23:09 +0900

    Add attributes of base entity (createdAt, updatedAt) for auditing

--- core/src/main/java/com/myfin/core/base/BaseEntity.java
+++ core/src/main/java/com/myfin/core/base/BaseEntity.java
@@ -1,4 +1,39 @@
 package com.myfin.core.base;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public class BaseEntity {
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = SeoulDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = SeoulDateTime.now();
+    }
+
 }
