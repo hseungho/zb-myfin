b16d8a5c649ccc41144d760172b7bdfdf86dad1c
hseungho <hsh47607@naver.com>
2023-07-18 15:25:15 +0900

    Chore add comments of checkuserid available funtion

--- module-api/src/main/java/com/myfin/api/service/UserCheckService.java
+++ module-api/src/main/java/com/myfin/api/service/UserCheckService.java
@@ -2,6 +2,11 @@ package com.myfin.api.service;
 
 public interface UserCheckService {
 
+    /**
+     * 아이디 사용가능 여부 확인.
+     * @param userId 확인할 유저 아이디
+     * @return 사용가능하면 true
+     */
     boolean checkUserIdAvailable(String userId);
 
 }
