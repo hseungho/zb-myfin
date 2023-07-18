f2fba81304eb55574a6df454e6b987ef9eb6cecd
hseungho <hsh47607@naver.com>
2023-07-18 14:11:04 +0900

    Add column annotation to each field of user class

--- core/src/main/java/com/myfin/core/entity/User.java
+++ core/src/main/java/com/myfin/core/entity/User.java
@@ -1,8 +1,7 @@
 package com.myfin.core.entity;
 
 import com.myfin.core.type.SexType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,29 +18,43 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
+    @Column(name = "id", nullable = false, updatable = false)
     private String id;
 
+    @Column(name = "user_id", nullable = false, updatable = false)
     private String userId;
 
+    @Column(name = "user_pw", nullable = false)
     private String password;
 
+    @Column(name = "user_name", nullable = false)
     private String name;
 
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "sex_flag", nullable = false)
     private SexType sex;
 
+    @Column(name = "zip_code", nullable = false)
     private String zipCode;
 
+    @Column(name = "address_1", nullable = false)
     private String address1;
 
+    @Column(name = "address_2")
     private String address2;
 
+    @Column(name = "phone_num", nullable = false)
     private String phoneNum;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "last_logged_in_at")
     private LocalDateTime lastLoggedInAt;
 
+    @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 }
