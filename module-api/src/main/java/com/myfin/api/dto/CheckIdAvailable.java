99032e4d632df7aae8a9c511075a5255d47a63c8
hseungho <hsh47607@naver.com>
2023-07-18 15:58:59 +0900

    Add @NotBlank for CheckUserIdAvailabe request data

--- module-api/src/main/java/com/myfin/api/dto/CheckIdAvailable.java
+++ module-api/src/main/java/com/myfin/api/dto/CheckIdAvailable.java
@@ -1,5 +1,6 @@
 package com.myfin.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ public class CheckIdAvailable {
     @Data
     @AllArgsConstructor(staticName = "of")
     public static class Response {
+        @NotBlank
         private boolean result;
     }
 
