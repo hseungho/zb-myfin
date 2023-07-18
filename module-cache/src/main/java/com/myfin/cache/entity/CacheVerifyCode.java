8ba3e17a50e2d332b40f01ccef54de2474a03544
hseungho <hsh47607@naver.com>
2023-07-18 18:41:59 +0900

    Add Cache entity

---
+++ module-cache/src/main/java/com/myfin/cache/entity/CacheVerifyCode.java
@@ -0,0 +1,36 @@
+package com.myfin.cache.entity;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.Objects;
+
+@Getter
+@RedisHash(value = "VerifyCode", timeToLive = 180L) // TTL: 3분
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE, staticName = "of")
+public class CacheVerifyCode {
+
+    @Id
+    private String phoneNum;
+
+    private String code;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CacheVerifyCode that = (CacheVerifyCode) o;
+        return Objects.equals(phoneNum, that.phoneNum);
+    }
+
+    @Override
+    public int hashCode() {
+        return phoneNum != null ? phoneNum.hashCode() : 0;
+    }
+
+}
