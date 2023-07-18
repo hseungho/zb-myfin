063e46e78bc278ad5c2eb7b01edd3a0bc93210a5
hseungho <hsh47607@naver.com>
2023-07-18 14:06:50 +0900

    Add attributes of user class

--- core/src/main/java/com/myfin/core/entity/User.java
+++ core/src/main/java/com/myfin/core/entity/User.java
@@ -1,4 +1,46 @@
 package com.myfin.core.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public class User {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String password;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private SexType sex;
+
+    private String zipCode;
+
+    private String address1;
+
+    private String address2;
+
+    private String phoneNum;
+
+    private String email;
+
+    private LocalDateTime lastLoggedInAt;
+
+    private LocalDateTime deletedAt;
 }
