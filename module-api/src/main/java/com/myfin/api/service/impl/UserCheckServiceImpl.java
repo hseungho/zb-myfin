336f85b045e286d48b90a4070f1cf1a47e09ff03
hseungho <hsh47607@naver.com>
2023-07-18 15:24:05 +0900

    Add function for check user id available

--- module-api/src/main/java/com/myfin/api/service/impl/UserCheckServiceImpl.java
+++ module-api/src/main/java/com/myfin/api/service/impl/UserCheckServiceImpl.java
@@ -5,6 +5,7 @@ import com.myfin.core.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -14,8 +15,9 @@ public class UserCheckServiceImpl implements UserCheckService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public boolean checkUserIdAvailable(String userId) {
-        return false;
+        return !userRepository.existsByUserId(userId);
     }
 
 }
