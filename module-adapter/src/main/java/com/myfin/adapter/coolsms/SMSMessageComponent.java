2ee1abb9f1e3ecf272b66cb5cc8ad9753f6b9029
hseungho <hsh47607@naver.com>
2023-07-18 18:01:34 +0900

    Add feature of send messsage using NurigoAPI

---
+++ module-adapter/src/main/java/com/myfin/adapter/coolsms/SMSMessageComponent.java
@@ -0,0 +1,46 @@
+package com.myfin.adapter.coolsms;
+
+import lombok.extern.slf4j.Slf4j;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SMSMessageComponent {
+
+    private final DefaultMessageService messageService;
+    private final String fromNumber;
+
+    public SMSMessageComponent(@Value("${cool-sms.api-key}") final String apiKey,
+                               @Value("${cool-sms.api-secret-key}") final String apiSecretKey,
+                               @Value("${cool-sms.domain}") final String domain,
+                               @Value("${cool-sms.from-number}") final String fromNumber) {
+
+        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, domain);
+        this.fromNumber = fromNumber;
+
+    }
+
+    public void sendMessage(final String toNumber, final String verifyCode) {
+        Message message = new Message();
+        message.setFrom(this.fromNumber);
+        message.setTo(toNumber.replace("-", ""));
+        message.setText("[마이핀페이] 본인확인 인증번호["+verifyCode+"]를 화면에 입력해주세요.");
+
+        try {
+            this.messageService.send(message);
+
+        } catch (Exception e) {
+            final String toNum = toNumber.replace("-", "")
+                            .replace(toNumber.substring(3, 7), "****");
+            
+            log.error("Occurred Exception during send SMS to "+toNum+".", e);
+
+            throw new RuntimeException();
+        }
+    }
+
+}
