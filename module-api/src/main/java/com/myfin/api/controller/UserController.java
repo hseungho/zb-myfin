4e515b7a56f6024da9b5cdb68ea932e8f98cfc9b
hseungho <hsh47607@naver.com>
2023-07-18 15:17:00 +0900

    Add check id api endpoint of UserController

--- module-api/src/main/java/com/myfin/api/controller/UserController.java
+++ module-api/src/main/java/com/myfin/api/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.myfin.api.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping("/sign-up/check-id")
+    @ResponseStatus(HttpStatus.OK)
+    public CheckIdAvailable.Response checkUserIdAvailable(@RequestParam("key") String userId) {
+        return CheckIdAvailable.Response.of();
+    }
+
 }
