02a70e5060c10d13b62abf61d8e6b34b40a2c280
hseungho <hsh47607@naver.com>
2023-07-18 16:47:48 +0900

    Add validation for check userid available

--- module-api/src/main/java/com/myfin/api/service/impl/UserCheckServiceImpl.java
+++ module-api/src/main/java/com/myfin/api/service/impl/UserCheckServiceImpl.java
@@ -2,6 +2,7 @@ package com.myfin.api.service.impl;
 
 import com.myfin.api.service.ATopServiceComponent;
 import com.myfin.api.service.UserCheckService;
+import com.myfin.core.exception.impl.BadRequestException;
 import com.myfin.core.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,9 @@ public class UserCheckServiceImpl extends ATopServiceComponent implements UserCh
     @Override
     @Transactional(readOnly = true)
     public boolean checkUserIdAvailable(String userId) {
+        if (hasNotTexts(userId)) {
+            throw new BadRequestException("중복확인할 아이디를 입력해주세요.");
+        }
         return !userRepository.existsByUserId(userId);
     }
 
