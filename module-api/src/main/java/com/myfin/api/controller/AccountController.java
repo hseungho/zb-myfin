315923adad05c272b3ae2c378323520651cdc197
hseungho <hsh47607@naver.com>
2023-07-20 23:31:32 +0900

    Add account method inside AccountController
    Signed-off-by: hseungho <hsh47607@naver.com>

--- module-api/src/main/java/com/myfin/api/controller/AccountController.java
+++ module-api/src/main/java/com/myfin/api/controller/AccountController.java
@@ -1,14 +1,20 @@
 package com.myfin.api.controller;
 
+import com.myfin.api.dto.CreateAccount;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class AccountController {
 
-    
+    @PostMapping("/accounts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateAccount.Response createAccount(@RequestBody @Valid CreateAccount.Request request) {
+        return CreateAccount.Response.fromDto();
+    }
 
 }
---
+++ module-api/src/main/java/com/myfin/api/dto/CreateAccount.java
@@ -0,0 +1,63 @@
+package com.myfin.api.dto;
+
+import com.myfin.core.dto.AccountDto;
+import com.myfin.core.dto.UserDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+public class CreateAccount {
+    @Data @NoArgsConstructor @AllArgsConstructor @Builder
+    public static class Request {
+        @NotNull(message = "계좌비밀번호를 입력해주세요")
+        @NotBlank(message = "계좌비밀번호를 입력해주세요")
+        private String accountPassword;
+        private Long initialBalance;
+    }
+
+    @EqualsAndHashCode(callSuper = true)
+    @Data @NoArgsConstructor @AllArgsConstructor @Builder
+    public static class Response extends ATopResponse {
+        private UserResponse user;
+        private AccountResponse account;
+
+        public static Response fromDto(AccountDto dto) {
+            return Response.builder()
+                    .user(UserResponse.fromDto(dto.getOwner()))
+                    .account(AccountResponse.fromDto(dto))
+                    .build();
+        }
+
+        @Data @NoArgsConstructor @AllArgsConstructor @Builder
+        private static class UserResponse {
+            private String name;
+            private String phoneNum;
+            private String email;
+            private String createdAt;
+
+            private static UserResponse fromDto(UserDto dto) {
+                return UserResponse.builder()
+                        .name(dto.getName())
+                        .phoneNum(dto.getPhoneNum())
+                        .email(dto.getEmail())
+                        .createdAt(getDateTimeIfPresent(dto.getCreatedAt()))
+                        .build();
+            }
+        }
+
+        @Data @NoArgsConstructor @AllArgsConstructor @Builder
+        private static class AccountResponse {
+            private String number;
+            private long balance;
+            private String createdAt;
+
+            private static AccountResponse fromDto(AccountDto dto) {
+                return AccountResponse.builder()
+                        .number(dto.getNumber())
+                        .balance(dto.getBalance())
+                        .createdAt(getDateTimeIfPresent(dto.getCreatedAt()))
+                        .build();
+            }
+        }
+    }
+}
