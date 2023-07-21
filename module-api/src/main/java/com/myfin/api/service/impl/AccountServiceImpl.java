b3bb162bcfcda454ee0bd010aa2964be737ca890
hseungho <hsh47607@naver.com>
2023-07-22 01:01:43 +0900

    Add AccountService interface and class
    Signed-off-by: hseungho <hsh47607@naver.com>

---
+++ module-api/src/main/java/com/myfin/api/service/impl/AccountServiceImpl.java
@@ -0,0 +1,19 @@
+package com.myfin.api.service.impl;
+
+import com.myfin.api.dto.CreateAccount;
+import com.myfin.api.service.AccountService;
+import com.myfin.core.dto.AccountDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+
+    @Override
+    public AccountDto createAccount(CreateAccount.Request request) {
+        return null;
+    }
+}
