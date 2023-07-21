069b83388318cc608d01fbd0fad8620d491d2a4c
hseungho <hsh47607@naver.com>
2023-07-22 01:05:02 +0900

    Inherit ATopServiceComponent from AccountServiceImpl
    Signed-off-by: hseungho <hsh47607@naver.com>

--- module-api/src/main/java/com/myfin/api/service/impl/AccountServiceImpl.java
+++ module-api/src/main/java/com/myfin/api/service/impl/AccountServiceImpl.java
@@ -1,6 +1,7 @@
 package com.myfin.api.service.impl;
 
 import com.myfin.api.dto.CreateAccount;
+import com.myfin.api.service.ATopServiceComponent;
 import com.myfin.api.service.AccountService;
 import com.myfin.core.dto.AccountDto;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +11,11 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class AccountServiceImpl implements AccountService {
+public class AccountServiceImpl extends ATopServiceComponent implements AccountService {
 
     @Override
     public AccountDto createAccount(CreateAccount.Request request) {
         return null;
     }
+
 }
