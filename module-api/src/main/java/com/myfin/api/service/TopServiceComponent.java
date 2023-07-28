f4b99e681eb6fe21e3b7e0035beaf00126b1d6fb
hseungho <hsh47607@naver.com>
2023-07-29 02:04:20 +0900

    Modify comments and import sente.
    Signed-off-by: hseungho <hsh47607@naver.com>

--- module-api/src/main/java/com/myfin/api/service/TopServiceComponent.java
+++ module-api/src/main/java/com/myfin/api/service/TopServiceComponent.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 
 /**
- * ATopServiceComponent 클래스는 Service 클래스들의 최상위 클래스로써, <br>
+ * TopServiceComponent 클래스는 Service 클래스들의 최상위 클래스로써, <br>
  * 다수의 Service 클래스에서 사용되는 기능들을 정의하도록 한 클래스이다.
  */
 public class TopServiceComponent {
