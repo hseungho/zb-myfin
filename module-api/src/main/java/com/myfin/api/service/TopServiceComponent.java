5ced7bbfb10b432de31bc7c6a7de6bff744051fd
hseungho <hsh47607@naver.com>
2023-07-30 23:44:31 +0900

    Add SecurityUtil class for security util method
    Signed-off-by: hseungho <hsh47607@naver.com>

--- module-api/src/main/java/com/myfin/api/service/TopServiceComponent.java
+++ module-api/src/main/java/com/myfin/api/service/TopServiceComponent.java
@@ -1,29 +1,8 @@
 package com.myfin.api.service;
 
-import com.myfin.core.entity.User;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 /**
  * TopServiceComponent 클래스는 Service 클래스들의 최상위 클래스로써, <br>
  * 다수의 Service 클래스에서 사용되는 기능들을 정의하도록 한 클래스이다.
  */
 public class TopServiceComponent {
-
-
-    /**
-     * 로그인 유저의 `User` 객체를 반환.
-     * @return 로그인 유저의 `User` 객체
-     */
-    protected User loginUser() {
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
-
-    /**
-     * 로그인 유저의 PK ID를 반환.
-     * @return 로그인 유저의 PK ID
-     */
-    protected String loginId() {
-        return this.loginUser().getId();
-    }
-
 }
