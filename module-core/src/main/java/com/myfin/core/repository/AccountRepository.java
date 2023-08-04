89b96db18a72392d34a97d15fd1ce916c5f297d4
hseungho <hsh47607@naver.com>
2023-08-04 23:47:48 +0900

    Fix query of find account that excluded deletion
    Signed-off-by: hseungho <hsh47607@naver.com>

--- module-core/src/main/java/com/myfin/core/repository/AccountRepository.java
+++ module-core/src/main/java/com/myfin/core/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.myfin.core.repository;
 import com.myfin.core.entity.Account;
 import com.myfin.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,5 +15,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     boolean existsByNumber(String number);
 
+    @Query(value = "SELECT a FROM account a WHERE a.deletedAt = null ")
     Optional<Account> findByNumber(String number);
 }
